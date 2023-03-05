c3418a5aa9ab7bc7e2181f0daddaf2275bc1e754
serjlemast <maksiomrpp@gmail.com>
2023-03-05 17:01:13 +0200

    Finish Task number 1

--- Maksymov_IKM_721B_project/Form1.Designer.cs
+++ Maksymov_IKM_721B_project/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            bStart = new Button();
+            tbInput = new TextBox();
+            tClock = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(266, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Some text";
+            label1.Click += label1_Click;
+            // 
+            // bStart
+            // 
+            bStart.BackColor = SystemColors.ActiveBorder;
+            bStart.Location = new Point(266, 120);
+            bStart.Name = "bStart";
+            bStart.Size = new Size(75, 23);
+            bStart.TabIndex = 1;
+            bStart.Text = "Пуск";
+            bStart.UseVisualStyleBackColor = false;
+            bStart.Click += button1_Click;
+            // 
+            // tbInput
+            // 
+            tbInput.Enabled = false;
+            tbInput.Location = new Point(254, 91);
+            tbInput.Name = "tbInput";
+            tbInput.Size = new Size(100, 23);
+            tbInput.TabIndex = 2;
+            tbInput.Text = "tbInput";
+            tbInput.TextChanged += textBox1_TextChanged;
+            tbInput.KeyPress += tbInput_KeyPress;
+            // 
+            // tClock
+            // 
+            tClock.Interval = 25000;
+            tClock.Tick += tClock_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tbInput);
+            Controls.Add(bStart);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button bStart;
+        private TextBox tbInput;
+        private System.Windows.Forms.Timer tClock;
     }
 }
